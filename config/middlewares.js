392c2fcee1c1ea6c2b10fa1fa89ec6099ee5d641
edevars <enrique.devars@storicard.com>
2024-04-09 12:17:19 -0600

    feat: starting strapi aws uploader

---
+++ config/middlewares.js
@@ -0,0 +1,39 @@
+require('dotenv').config()
+
+module.exports = [
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            process.env.AWS_CONFIGURATION_URI, 
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            process.env.AWS_CONFIGURATION_URI,
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
