db6722002d971e64623ec2f3f830446d518e4f1f
edevars <enrique.devars@storicard.com>
2024-04-10 12:23:25 -0600

    feat: adding private read

--- config/plugins.js
+++ config/plugins.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
         secretAccessKey: env('AWS_ACCESS_SECRET'),
         region: env('AWS_REGION'),
         params: {
-          ACL: env('AWS_ACL', 'public-read'),
+          ACL: 'private',
           signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
           Bucket: env('AWS_BUCKET'),
         },
